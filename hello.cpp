e881d786098fbc729ca7aceb2f6dbc41e03158bd
sudo-justinwilson <sudo.justin.wilson@gmail.com>
2017-02-17 13:00:34 +1100

    start Google course

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+// hello.cpp: Justin Wilson
+// Description: C++ hello world
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
---
+++ hello_for6.cpp
@@ -0,0 +1,16 @@
+// hello.cpp: Justin Wilson
+// Description: C++ hello world
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, i;
+  x = 6;
+  for (i=0; i <= x; i++)
+  {
+    cout << "Hello World!" << endl;
+    }
+  return 0;
+}
+
